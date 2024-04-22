9656d1cfa92db90001696ba0012345d0d93b6d61
Tushar Sharma <tttushar.789@gmail.com>
2024-04-22 16:03:43 +0530

    first commit

---
+++ src/app/aboutus/page.js
@@ -0,0 +1,5 @@
+function AboutUs() {
+  return <h1>The About Page</h1>;
+}
+
+export default AboutUs;
---
+++ src/app/news/page.js
@@ -0,0 +1,5 @@
+function NewsPage() {
+  return <h1>The News page</h1>;
+}
+
+export default NewsPage;
