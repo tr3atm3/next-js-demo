813e9cba2b6a261d57e3a060aa7f7d05f7b1dab2
Tushar Sharma <tttushar.789@gmail.com>
2024-04-23 13:20:13 +0530

    fourth commit

--- src/app/aboutus/page.js
+++ src/app/aboutus/page.js
@@ -1,5 +1,26 @@
+import Link from "next/link";
+
+const details = [
+  { id: 1, name: "Yash", role: "Senior Developer" },
+
+  { id: 2, name: "Vaibhav", role: "Backend Developer" },
+
+  { id: 3, name: "Suresh", role: "Frontend Developer" },
+];
+
 function AboutUs() {
-  return <h1>The About Page</h1>;
+  return (
+    <div>
+      <h1>The About Page</h1>;
+      <ul>
+        {details.map((dev) => (
+          <Link key={dev.id} href={`/aboutus/${dev.id}`}>
+            <li>{dev.name}</li>
+          </Link>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default AboutUs;
